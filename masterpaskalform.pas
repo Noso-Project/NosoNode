707bdd850e88eaff0f73e440cf200c4b09b3728c
Pedro alicia <fumigadoraconstelacion90@gmail.com>
2021-12-30 09:36:30 -0400

    Official Release 0.2.1Ka7

--- masterpaskalform.pas
+++ masterpaskalform.pas
@@ -664,7 +664,7 @@ CONST
                             '185.239.239.184 '+
                             '109.230.238.240';
   ProgramVersion = '0.2.1';
-  SubVersion = 'Ka6';
+  SubVersion = 'Ka7';
   OficialRelease = true;
   BuildDate = 'December 2021';
   ADMINHash = 'N4PeJyqj8diSXnfhxSQdLpo8ddXTaGd';
