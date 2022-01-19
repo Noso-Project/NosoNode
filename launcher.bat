592bee3e083015f1c1fd4ccb5db1eb488e66d0e3
Pedro alicia <fumigadoraconstelacion90@gmail.com>
2022-01-19 00:12:15 -0400

    launcher.bat gitignore

--- launcher.bat
+++
@@ -1,7 +0,0 @@
-echo Restarting Noso...
-TIMEOUT 5
-tasklist /FI "IMAGENAME eq Noso.exe" 2>NUL | find /I /N "Noso.exe">NUL
-if "%ERRORLEVEL%"=="0" taskkill /F /im Noso.exe
-del noso.exe
-ren nosonew noso.exe
-start noso.exe
