f913611318ff5af62a49649c7f228489ba89bd66
Andrii Pasichnik 🇺🇦 <67899666+pasichDev@users.noreply.github.com>
2024-09-12 23:58:12 +0300

    Update nosonosocfg.pas

--- nosonosocfg.pas
+++ nosonosocfg.pas
@@ -40,7 +40,7 @@ var
   LasTimeCFGRequest : int64 = 0;
   DefaultNosoCFG    : String = // CFG parameters
                             {0 Mainnet mode}'NORMAL '+
-                            {1 Seed nodes  }'204.10.194.22;8080:204.10.194.29;8080:204.10.194.32;8080:204.10.194.36;8080:204.10.194.33;8080:'+
+                            {1 Seed nodes  }'204.10.194.22;8080:204.10.194.29;8080:204.10.194.32;8080:204.10.194.36;8080:204.10.194.33;8080: '+
                             {2 NTP servers }'ts2.aco.net:hora.roa.es:time.esa.int:time.stdtime.gov.tw:stratum-1.sjc02.svwh.net:ntp1.sp.se:1.de.pool.ntp.org:ntps1.pads.ufrj.br:utcnist2.colorado.edu:tick.usask.ca:ntp1.st.keio.ac.jp: ' +
                             {3 DEPRECATED  }'null: '+
                             {4 DEPRECATED  }'null: '+
