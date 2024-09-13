bfbff0b1f3af0396cc99528ca988f61705b82051
Andrii Pasichnik 🇺🇦 <67899666+pasichDev@users.noreply.github.com>
2024-09-13 09:46:17 +0300

    Update masterpaskalform.pas

--- masterpaskalform.pas
+++ masterpaskalform.pas
@@ -573,7 +573,7 @@ CONST
   RestartFileName = 'launcher.sh';
   updateextension = 'tgz';
   {$ENDIF}
-  NodeRelease = 'Ab5';
+  NodeRelease = 'Ab6';
   OficialRelease = true;
   BetaRelease    = false;
   VersionRequired = '0.4.4';
